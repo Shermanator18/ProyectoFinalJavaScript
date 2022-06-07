c5f020cdd4dbe69abc82ca36b5dba2fbd9e3aef5
Bellizzi <dbellizz@senado.gob.ar>
2022-06-07 12:09:57 -0300

    ValidacionIngreso

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const cuota = function (number,cantCuotas,interes){return number * cantCuotas * interes /**/};
 let number = Number(prompt("Ingrese Monto Total"));
+while (number<1){
+    let number = Number(prompt("Ingrese Monto Total"));
+}
 let cantCuotas = Number(prompt("Ingrese cantidad de cuotas en las que va a pagar"));
+while(cantCuotas<2){
+    let cantCuotas = Number(prompt("Ingrese cantidad de cuotas en las que va a pagar"));
+}
 let interes = Number(prompt("Ingrese interes a cobrar"));
 console.log(cuota(number,cantCuotas,interes));
