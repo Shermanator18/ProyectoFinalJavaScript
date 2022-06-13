ec5d5c4894b44c17b5eddcaf8acfd63090580e42
Bellizzi <dbellizz@senado.gob.ar>
2022-06-13 10:58:15 -0300

    FuncionDentrodelbuttonclick

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const cuota = function (number,cantCuotas,interes){return number * cantCuotas * interes /**/};
+/* const cuota = function (number,cantCuotas,interes){return number * cantCuotas * interes };
 let number = Number(prompt("Ingrese Monto Total"));
 while (number<1){
     let number = Number(prompt("Ingrese Monto Total"));
@@ -9,3 +9,24 @@ while(cantCuotas<2){
 }
 let interes = Number(prompt("Ingrese interes a cobrar"));
 console.log(cuota(number,cantCuotas,interes));
+ */
+function funcion() {
+    const total =   document.getElementById("monTot").value;
+    const cuotas = document.getElementById("cuotas").value;
+    const interes = document.getElementById("interes").value;
+    if (total <2){
+        alert("Error ingrese un total Valido");
+        return;
+    }
+    if(cuotas <2){
+        alert("Error ingrese cuotas validas");
+        return;
+    }
+    if(interes <2){
+        alert("Error ingrese interes valido");
+        return;
+    }
+    const arrayGeneral =[total,cuotas,interes];
+    let resultado = arrayGeneral[0] / arrayGeneral[1] + arrayGeneral[2]/arrayGeneral[1];
+    alert(resultado);
+}
