e30207b7639a61f4e211c00139b72146ec371ee2
Bellizzi <dbellizz@senado.gob.ar>
2022-06-15 11:41:27 -0300

    LimpiarCamposError

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
-/* const cuota = function (number,cantCuotas,interes){return number * cantCuotas * interes };
-let number = Number(prompt("Ingrese Monto Total"));
-while (number<1){
-    let number = Number(prompt("Ingrese Monto Total"));
-}
-let cantCuotas = Number(prompt("Ingrese cantidad de cuotas en las que va a pagar"));
-while(cantCuotas<2){
-    let cantCuotas = Number(prompt("Ingrese cantidad de cuotas en las que va a pagar"));
-}
-let interes = Number(prompt("Ingrese interes a cobrar"));
-console.log(cuota(number,cantCuotas,interes));
- */
 function funcion() {
     let total =   document.getElementById("monTot").value;
     let cuotas = document.getElementById("cuotas").value;
     let interes = document.getElementById("interes").value;
     if (total <2){
         alert("Error ingrese un total Valido");
+        total = document.getElementById("monTot").value = "";
         return;
     }
     if(cuotas <2){
         alert("Error ingrese cuotas validas");
+        cuotas = document.getElementById("cuotas").value = "";       
         return;
     }
     if(interes <2){
         alert("Error ingrese interes valido");
+        interes = document.getElementById("interes").value = "";
         return;
     }
     const arrayGeneral =[total,cuotas,interes];
