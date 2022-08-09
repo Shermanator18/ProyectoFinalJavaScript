abe70d68541ec21910543d163c56a13a927bee49
Bellizzi <dbellizz@senado.gob.ar>
2022-08-09 16:40:06 -0300

    Eventos/DOM

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-function funcion() {
-    let total =   document.getElementById("monTot").value;
+let miform = document.getElementById("formulario");
+miform.addEventListener("submit", validarForm);
+    function validarForm(e){
+        e.preventDefault();
+        let formulario = e.target;
+        let total =  formulario.children[1].value;
+        let cuotas =  formulario.children[3].value;
+        let interes =  formulario.children[5].value;
+        if (total <2){
+            alert("Error ingrese un total Valido");
+            return;
+        }
+        if(cuotas <2){
+            alert("Error ingrese cuotas validas");
+            return;
+        }
+        if(interes <2){
+            alert("Error ingrese interes valido");
+            return;
+        }
+        const arrayGeneral =[total,cuotas,interes,0];
+        let resultado = arrayGeneral[0] / arrayGeneral[1] + arrayGeneral[2]/arrayGeneral[1];
+        arrayGeneral[4]= resultado;
+        alert(arrayGeneral[4]);
+    }
+    /*let total =   document.getElementById("monTot").value;
     let cuotas = document.getElementById("cuotas").value;
     let interes = document.getElementById("interes").value;
     if (total <2){
@@ -21,4 +45,4 @@ function funcion() {
     let resultado = arrayGeneral[0] / arrayGeneral[1] + arrayGeneral[2]/arrayGeneral[1];
     arrayGeneral[4]= resultado;
     alert(arrayGeneral[4]);
-}
+    */
