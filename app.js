ea686aefe95266b0b196ee4786aaea7db2cf3667
Bellizzi <dbellizz@senado.gob.ar>
2022-08-16 16:03:49 -0300

    Final

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+class Usuario {
+    constructor(obj) {
+        this.nombre  = obj.Usuario.toUpperCase();
+        this.pass  = obj.Usuario.toUpperCase();
+    }
+}
 let miform = document.getElementById("formulario");
 miform.addEventListener("submit", validarForm);
-    function validarForm(e){
+let frmlogin = document.getElementById("formLogin");    
+frmlogin.addEventListener("submit" , validarLogin);
+function validarForm(e){
         e.preventDefault();
         let formulario = e.target;
         let total =  formulario.children[1].value;
@@ -18,7 +26,7 @@ miform.addEventListener("submit", validarForm);
             alert("Error ingrese interes valido");
             return;
         }
-        let  arrayIngreso =[total,cuotas,interes,0];
+        let  arrayIngreso =[total,cuotas,interes];
         let  arrResultados=[0,0,0];
         /*Calculo el interes mensual*/
         arrResultados[0] = arrayIngreso[2] / 12;
@@ -27,7 +35,26 @@ miform.addEventListener("submit", validarForm);
         /*Calculo el total a pagar*/
         arrResultados[2] = arrResultados[1] * arrayIngreso[1];
         /*Muestra resultados*/
+        /*Traigo UserInfo**/
+        //Obtenemos el listado  almacenado
+        /* const almacenados = JSON.parse(localStorage.getItem("User"));
+        const usuarios = [];
+        for (const objeto of almacenados){
+            usuarios.push(new Usuariooo(objeto));
+        }*/
+        let usuario;
+        let usuarioEnLS = JSON.stringify(localStorage.getItem("User"));
+        alert("Hola "+usuarioEnLS);
         document.getElementById('totcuota').innerHTML = 'El valor de cada Cuota es de: '+parseFloat(arrResultados[1]).toFixed(2);
         document.getElementById('total').innerHTML = 'El valor total a pagar es de: '+parseFloat(arrResultados[2]).toFixed(2);
-        alert(arrResultados[2]);
     }
+function validarLogin(e){
+    e.preventDefault();
+    let formm = e.target;
+    let nomb =  frmlogin.children[1].value;
+    let pass =  frmlogin.children[3].value;
+    const usuario = { id: 1,  nombre: nomb, contra: pass };
+    const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+    guardarLocal("User", JSON.stringify(usuario));
+    document.getElementById("formulario").style.display="block";
+}
