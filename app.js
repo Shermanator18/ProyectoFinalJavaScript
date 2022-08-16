a3fc1bacf94b7beb4aee29c0126151699dc6a70e
Bellizzi <dbellizz@senado.gob.ar>
2022-08-16 12:29:03 -0300

    Modificaciones

--- app.js
+++ app.js
@@ -18,31 +18,16 @@ miform.addEventListener("submit", validarForm);
             alert("Error ingrese interes valido");
             return;
         }
-        const arrayGeneral =[total,cuotas,interes,0];
-        let resultado = arrayGeneral[0] / arrayGeneral[1] + arrayGeneral[2]/arrayGeneral[1];
-        arrayGeneral[4]= resultado;
-        alert(arrayGeneral[4]);
+        let  arrayIngreso =[total,cuotas,interes,0];
+        let  arrResultados=[0,0,0];
+        /*Calculo el interes mensual*/
+        arrResultados[0] = arrayIngreso[2] / 12;
+        /*Calculo el valor de cada cuota*/
+        arrResultados[1] = (arrayIngreso[0] / arrayIngreso[1])*arrResultados[0];
+        /*Calculo el total a pagar*/
+        arrResultados[2] = arrResultados[1] * arrayIngreso[1];
+        /*Muestra resultados*/
+        document.getElementById('totcuota').innerHTML = 'El valor de cada Cuota es de: '+parseFloat(arrResultados[1]).toFixed(2);
+        document.getElementById('total').innerHTML = 'El valor total a pagar es de: '+parseFloat(arrResultados[2]).toFixed(2);
+        alert(arrResultados[2]);
     }
-    /*let total =   document.getElementById("monTot").value;
-    let cuotas = document.getElementById("cuotas").value;
-    let interes = document.getElementById("interes").value;
-    if (total <2){
-        alert("Error ingrese un total Valido");
-        total = document.getElementById("monTot").value = "";
-        return;
-    }
-    if(cuotas <2){
-        alert("Error ingrese cuotas validas");
-        cuotas = document.getElementById("cuotas").value = "";       
-        return;
-    }
-    if(interes <2){
-        alert("Error ingrese interes valido");
-        interes = document.getElementById("interes").value = "";
-        return;
-    }
-    const arrayGeneral =[total,cuotas,interes,0];
-    let resultado = arrayGeneral[0] / arrayGeneral[1] + arrayGeneral[2]/arrayGeneral[1];
-    arrayGeneral[4]= resultado;
-    alert(arrayGeneral[4]);
-    */
